da0f70cde18c0fe79ae6ed33d485212b2fb0391b
OA Wu <comdan66@gmail.com>
2017-09-01 19:54:10 +0800

    Fix code.

--- views/_menu.php
+++ views/_menu.php
@@ -6,7 +6,11 @@
 
   <a href="<?php echo PAGE_URL_INDEX;?>" class="icon-h<?php echo isset ($now) && $now == 'index' ? ' a': '';?>">網站首頁</a>
   <a href="<?php echo PAGE_URL_DEVS;?>" class="icon-t<?php echo isset ($now) && $now == 'devs' ? ' a': '';?>" data-cnt="<?php echo number_format (count ($devs));?>">開發心得</a>
-  <a href="<?php echo PAGE_URL_BLOGS;?>" class="icon-b<?php echo isset ($now) && $now == 'blogs' ? ' a': '';?>" data-cnt="<?php echo number_format (count ($blogs));?>">生活紀錄</a>
+  <?php
+  if ($blogs) { ?>
+    <a href="<?php echo PAGE_URL_BLOGS;?>" class="icon-b<?php echo isset ($now) && $now == 'blogs' ? ' a': '';?>" data-cnt="<?php echo number_format (count ($blogs));?>">生活紀錄</a>
+<?php
+  } ?>
   <a href="<?php echo PAGE_URL_UNBOXINGS;?>" class="icon-g<?php echo isset ($now) && $now == 'unboxings' ? ' a': '';?>" data-cnt="<?php echo number_format (count ($unboxings));?>">開箱文章</a>
   <a href="<?php echo PAGE_URL_ALBUMS;?>" class="icon-i<?php echo isset ($now) && $now == 'albums' ? ' a': '';?>" data-cnt="<?php echo number_format (count ($albums));?>">個人相簿</a>
   <a href="<?php echo PAGE_URL_TIMELINE;?>" class="icon-v<?php echo isset ($now) && $now == 'timeline' ? ' a': '';?>">成就紀錄</a>
