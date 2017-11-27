61bf945e64535696a0d3955324664555db18ad30
OA Wu <comdan66@gmail.com>
2017-11-27 15:40:55 +0800

    Fix layout.

--- views/timeline.php
+++ views/timeline.php
@@ -2,7 +2,7 @@
   <div>
     <figure class="b tl">
       <div class="_ic">
-        <img src="https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/17966402_1653566597990009_108050767340849992_o.jpg?oh=9e485c8c81f7ac00b38e3f624d5fcde1&oe=59EC6083" />
+        <img src="img/timeline.jpg" />
       </div>
     </figure>
 
